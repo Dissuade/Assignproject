0fb5e63a9fd9a8af464e88252d32aef3f0078740
Dissuade <montrealgurpreet19@gmail.com>
2022-11-06 01:57:38 -0400

    First ever commit using spring initiliazer, git and source tree. First, i make a repo in github, copied the url in source tree, created the springboot project in spring initiliazer and extract all the files from it and copied in repository, after which all the changes showed in source tree. Stage all of them and do a commit.

---
+++ src/main/java/com/example/Assignproject/AssignprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assignproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Assignproject/AssignprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assignproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
